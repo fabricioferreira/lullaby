8f78486e19039f744cb7648560fc3db8f5738672
Carvalho, Fabricio <Fabricio_Carvalho@Dell.com>
2017-05-13 11:55:03 -0300

    Renamed project to something meaningful

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# unYoke
-unYoke is a REST enabler API that you can use to rapidly expose a data source as a RESTfull service.
+# Lullaby
+Lullaby is a REST enabler API that you can use to rapidly expose a data source as a RESTfull service.
--- src/db/provider-factory.ts
+++ src/db/provider-factory.ts
@@ -1,9 +1,20 @@
+// TODO: Find a way to dinamically load providers based on the desired type
+
 import { IProvider } from 'db/provider';
+import { SqlServerProvider } from 'providers/sql-server/sql-server-provider';
 
 export class ProviderFactory {
 	public constructor() { }
 
 	public createProvider(type: string): Promise<IProvider> {
-		return null;
+		var ret = new Promise<IProvider>(resolve => {
+			if (type === 'MSSQL') {
+				// resolve(new SqlServerProvider())
+
+				resolve(null);
+			}
+		});
+
+		return ret;
 	}
 }
